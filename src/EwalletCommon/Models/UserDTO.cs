138210d8f653b7bcf969eaf0325790319c8608e4
Dawid <dawid.pfv@gmail.com>
2017-04-05 06:41:23 +0200

    User model

---
+++ src/EwalletCommon/Models/UserDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EwalletCommon.Models
+{
+    public class UserDTO
+    {
+        /// <summary>
+        /// User id
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// User email which is also user login
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// User password
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
