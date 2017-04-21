1db3f306ca91fecb3a2402c6595ba589f4e93f54
Dawid <dawid.pfv@gmail.com>
2017-04-21 23:06:30 +0200

    Porządki

--- src/EwalletCommon/Utils/Exceptions.cs
+++ src/EwalletCommon/Utils/Exceptions.cs
@@ -6,4 +6,16 @@ using System.Threading.Tasks;
 namespace EwalletCommon.Utils
 {
     public class DuplicateUsernameException : Exception { }
+
+    public class ObjectAlreadyExists : Exception { }
+
+    public class NotUniqueException : Exception { }
+
+    public class NotFoundException : Exception { }
+
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        { }
+    }
 }
